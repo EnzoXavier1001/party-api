e0fec689221ebd24f57eab30864ad0752b18b0a2
Enzo Costa <enzo_costa7@hotmail.com>
2023-04-27 00:12:25 -0300

    feat: create dotenv with mongodb settings

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose")
 
 async function main() {
     try {
+        const username = process.env.USERNAME_MONGODB
+        const password = process.env.PASSWORD_MONGODB
+
         mongoose.set("strictQuery", true)
 
         await mongoose.connect(
-            "mongodb+srv://admin:T3NEdEEFBykgYZt7@cluster0.0amp3gk.mongodb.net/?retryWrites=true&w=majority"
+            `mongodb+srv://${username}:${password}@cluster0.0amp3gk.mongodb.net/?retryWrites=true&w=majority`
         )
 
         console.log("Conectado ao banco!");
